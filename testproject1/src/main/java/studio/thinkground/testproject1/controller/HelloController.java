6b3b9ebf33603ab7214636732581300286a39f85
kyscodeman <metalgos2@gmail.com>
2023-09-10 15:06:21 +0900

    부트스트램 제이쿼리 설정

--- testproject1/src/main/java/studio/thinkground/testproject1/controller/HelloController.java
+++ testproject1/src/main/java/studio/thinkground/testproject1/controller/HelloController.java
@@ -1,12 +1,12 @@
 package studio.thinkground.testproject1.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
 
         System.out.println("in hello con");
