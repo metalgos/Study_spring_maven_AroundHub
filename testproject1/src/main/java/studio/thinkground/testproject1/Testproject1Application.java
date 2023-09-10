f4169bd11f45eafc8207cbeead77b934b0be4f80
kyscodeman <metalgos2@gmail.com>
2023-09-10 14:39:20 +0900

    메이븐 + mysql + 머스태치 설정

--- testproject1/src/main/java/studio/thinkground/testproject1/Testproject1Application.java
+++ testproject1/src/main/java/studio/thinkground/testproject1/Testproject1Application.java
@@ -2,6 +2,7 @@ package studio.thinkground.testproject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Testproject1Application {
---
+++ testproject1/src/main/java/studio/thinkground/testproject1/controller/HelloController.java
@@ -0,0 +1,16 @@
+package studio.thinkground.testproject1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+
+        System.out.println("in hello con");
+        return "hello";
+    }
+}
